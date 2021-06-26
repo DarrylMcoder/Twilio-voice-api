b3ae55c2935fa0a67d4b1822eb7243ae4fdff4fe
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2021-06-26 07:57:19 -0400

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -10,3 +10,5 @@ $gather = $response->gather([
 $gather->say('Welcome, you have reached a Twilio test application by Darryl Martin. If you know your destination, please dial it now, otherwise, wait for the menu.');
 
 $gather->say('To speak directly to Darryl, press one. To send him a voice to text message, press two. For more of Darryl\'s telephone application projects, press three. Return to the previous menu at any time by pressing star. For the main menu, press pound.');
+
+echo $response;
