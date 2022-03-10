f6d0712c7e974b676c75dcf42e094f2ff4a509da
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-03-10 12:55:01 -0500

    Add files via upload

--- public/wiki.php
+++ public/wiki.php
@@ -3,21 +3,21 @@
 require('../vendor/autoload.php');
 use \Twilio\TwiML\VoiceResponse;
 
-$speechResult = $_POST['SpeechResult'];
+$speechResult = trim($_POST['SpeechResult'],'. ');
 $url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='.$speechResult.'&exintro=1&explaintext=1&exsectionformat=plain&format=json';
 
 $c = curl_init($url);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
 $json = curl_exec($c);
 $results = json_decode($json, true);
-$say = 'Hi. '. $speechResult.'. ';
+$say = '';
 
 foreach($results['query']['pages'] as $res){
   $say .= $res['extract'];
   
 }
 $response = new VoiceResponse();
-$response->say($json,['voice' => 'Polly.Matthew']);
+$response->say($say,['voice' => 'Polly.Matthew']);
 $response->pause(['length' => 3]);
 $response->redirect('main.php');
 echo $response;
