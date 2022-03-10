1035fb3e5bbdfacdac4151bc5fafaeed0206b8c5
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-03-10 07:31:03 -0500

    Add files via upload

--- public/wiki.php
+++ public/wiki.php
@@ -17,4 +17,6 @@ foreach($results['query']['pages'] as $res){
 }
 $response = new VoiceResponse();
 $response->say($say);
+$response->pause(['length' => 3]);
+$response->redirect('main.php');
 echo $response;
