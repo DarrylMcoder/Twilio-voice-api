78239a5e97c07a87e04b01cd858bf7b4821c7d16
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-03-15 07:27:53 -0400

    Add files via upload

--- public/commodities-api/error/invalid.php
+++ public/commodities-api/error/invalid.php
@@ -1,1 +1,12 @@
-
+<?PHP
+    
+require('../../vendor/autoload.php');
+include('../functions.php');
+include('../config.php');
+use \Twilio\TwiML\VoiceResponse;
+$response = new VoiceResponse();
+$from = $_POST['From'];
+$user = getUser($from,$mysqli);
+$response->say('Invalid input. Please try again.',['voice' => $user['voice']]);
+$response->redirect(dirname($_SERVER['HTTP_REFERER']).'/main.php');
+echo $response;
