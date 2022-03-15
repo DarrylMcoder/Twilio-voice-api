3a86a82c001cd0b1b502f85d1a955aed3c68e8ae
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-03-15 07:27:07 -0400

    Add files via upload

--- public/commodities-api/latest/index.php
+++ public/commodities-api/latest/index.php
@@ -1,1 +1,45 @@
+<?PHP
+    
+require('../../vendor/autoload.php');
+include('../functions.php');
+include('../config.php');
+use \Twilio\TwiML\VoiceResponse;
 
+$from = $_POST['From'];
+$user = getUser($from, $mysqli);
+$response = new VoiceResponse();
+
+$gather = $response->gather([
+  'action' => 'index.php',
+  'method' => 'post'
+]);
+
+$results = getCachedJSONArray('https://www.commodities-api.com/api/symbols?access_key='.getenv('COMMODITIES-API-ACCESS-KEY'), $user, 60*60*24*7);
+$numRes = array_values($results);
+
+$digits = $_REQUEST['Digits'];
+if(ctype_digit($digits)){
+  $item = array_search($numRes[$digits],$results);
+  header('Location: readPrice.php?item='.$item.'&index='.$digits);
+  exit;
+}
+
+if(strpos($digits,'*') === 0){
+  $index = trim($digits,'* #');
+}else{
+  $index = 1;
+}
+
+$gather->say('Dial the number of the commodity you want. I will read the list to you in a moment. You can dial star, followed by any number, to start reading at that item.',['voice' => $user['voice']]);
+
+for($i = $index; $i < count($results); $i++;){
+  if($i < 10){
+    $gather->say('For '.$numRes[$index].', dial '.$index.'.', ['voice' => $user['voice']]);
+  }else{
+    $gather->say('For '.$numRes[$index].', '.$index.'.', ['voice' => $user['voice']]);
+  }
+}
+
+$response->redirect('../');
+
+echo $response;
---
+++ public/commodities-api/latest/readPrice.php
@@ -0,0 +1,22 @@
+<?PHP
+    
+require('../../vendor/autoload.php');
+include('../functions.php');
+include('../config.php');
+
+use \Twilio\TwiML\VoiceResponse;
+$response = new VoiceResponse();
+
+$from = $_POST['From'];
+$user = getUser($from, $mysqli);
+$item = $_GET['item'];
+
+$currencies = getCachedJSONArray('https://www.commodities-api.com/api/symbols?access_key='.getenv('COMMODITIES-API-ACCESS-KEY'), $user, 60*60*24*7);
+
+$results = getCachedJSONArray('https://www.commodities-api.com/api/latest?access_key='.getenv('COMMODITIES-API-ACCESS-KEY').'&base='.$item.'&symbols='.$user['currency'], $user, 60*60*24);
+
+$response->say('As of '.date("F jS, Y", strtotime($results['data']['date'])).', according to data from commodities-api.com, the value of '.$results['data']['base'].' is '.$results['data']['rates'][$user['curency']].' '.$currencies[$user['currency']].' '.$results['data']['unit'].'.',['voice' => $user['voice']]);
+
+$response->redirect('index.php?Digits=*'.$_GET['index']);
+
+echo $response;
