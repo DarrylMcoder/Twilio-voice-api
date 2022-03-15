e87a5265b1b932a1b24dc860a0359072da0e0c1b
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-03-15 10:09:05 -0400

    Add files via upload

--- public/commodities-api/newUser.php
+++ public/commodities-api/newUser.php
@@ -8,9 +8,10 @@ include('config.php');
 use \Twilio\TwiML\VoiceResponse;
 $from = $_POST['From'];
 createUser($from,$mysqli);
+echo $mysqli->error;
 $response = new VoiceResponse();
 
-$response->say('An account has been created for you with 48 hours free trial period. After your trial has expired, you can choose to upgrade to a plan of 5 dollars per month, or continue using the limited version with paid features removed.',['voice' => 'Polly.Matthew']);
+$response->say('An account has been created for you with 24 hours free trial period. After your trial has expired, you can choose to upgrade to a plan of 5 dollars per month, or continue using the free version with limited access to some features.',['voice' => 'Polly.Matthew']);
 
 $response->redirect('index.php');
 
