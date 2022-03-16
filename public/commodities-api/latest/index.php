1fbaa90141c0da204a4d363e597b31c0a618dc8e
DarrylMcoder <80832451+DarrylMcoder@users.noreply.github.com>
2022-03-15 20:21:05 -0400

    Add files via upload

--- public/commodities-api/latest/index.php
+++ public/commodities-api/latest/index.php
@@ -1,7 +1,7 @@
 <?PHP
     
-ini_set('error_reporting', E_ALL ^ E_NOTICE); 
-ini_set('display_errors', 1);
+//ini_set('error_reporting', E_ALL ^ E_NOTICE); 
+//ini_set('display_errors', 1);
 
 require('../../../vendor/autoload.php');
 include('../functions.php');
