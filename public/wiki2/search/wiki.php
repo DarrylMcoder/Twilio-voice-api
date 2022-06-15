13927fcf0181f97fa0e40d2174f3f1c707c28d23
Darryl Martin <80832451+DarrylMcoder@users.noreply.github.com>
2022-06-15 18:11:29 -0400

    Add files via upload

--- public/wiki2/search/wiki.php
+++ public/wiki2/search/wiki.php
@@ -26,12 +26,14 @@ function get_wiki_page($title){
   $title = trim($title,'. ');
   $title = urlencode($title);
   $url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='.$title.'&exintro=1&explaintext=1&exsectionformat=plain&format=json';
-
   $c = curl_init($url);
   curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
   $json = curl_exec($c);
   $results = json_decode($json, true);
-  $page = $results['query']['pages'][0];
-  return $page;
+  $extract = '';
+  foreach($results['query']['pages'] as $page){
+   $extract .= $page['extract'];
+  }
+  return $extract;
 }
 echo $response;
