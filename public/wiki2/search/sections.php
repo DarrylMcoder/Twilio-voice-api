a60f31964d84d55c71adb20926a766f9d18e02ec
Darryl Martin <80832451+DarrylMcoder@users.noreply.github.com>
2022-06-16 13:28:38 -0400

    Add files via upload

--- public/wiki2/search/sections.php
+++ public/wiki2/search/sections.php
@@ -7,6 +7,25 @@ $response = new VoiceResponse();
 
 $number = $_REQUEST['Caller'];
 $user = new User($number);
+$title = $_GET['title'];
+$titles = $_GET['titles'];
+$titles = json_decode($titles, true);
+$sections = get_wiki_sections($title);
+
+$gather = $response->gather([
+  'action' => 'sections.php?title='.urlencode($title).'&titles='urlencode(json_encode($titles))
+]);
+
+foreach($titles as $number=>$section_title){
+  if($number === $_REQUEST['Digits']){
+    $gather->say($sections[$section_title],[
+      'voice' => $user->voice,
+      'language' => $user->language
+    ]);
+    $response->redirect("wiki.php?title=".urlencode($title));
+  }
+}
+
 switch($_REQUEST['Digits']){
   case '*':
     $response->redirect('input.php');
@@ -14,11 +33,37 @@ switch($_REQUEST['Digits']){
   case '#':
     $response->redirect('../welcome.php');
     break;
-  default:
-    $response->say('The number you entered was not valid.', [
-      'voice' => $user->voice,
-      'language' => $user->language
-    ]);
-    $response->redirect('input.php');
 }
+
+function get_wiki_sections($title){
+  $title = trim($title,'. ');
+  $title = urlencode($title);
+  $url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='.$title.'&exintro=1&explaintext=1&exsectionformat=plain&format=json';
+  $c = curl_init($url);
+  curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+  $json = curl_exec($c);
+  $results = json_decode($json, true);
+  $extract = '';
+  error_log($json);
+  foreach($results['query']['pages'] as $page){
+    error_log(json_encode($page));
+    $extract .= $page['extract'];
+  }
+  
+  $parts = explode('\n\n\n', $extract);
+  $sections = [];
+  $i = 0;
+  foreach($parts as $part){
+    $pair = explode('\n',$part, 2);
+    if(count($pair) === 2){
+      list($key, $val) = $pair;
+      $sections[$key] = $val;
+    }else{
+      $sections[$i] = $pair[0];
+      $i++;
+    }
+  }
+  return $sections;
+}
+
 echo $response;
--- public/wiki2/search/wiki.php
+++ public/wiki2/search/wiki.php
@@ -10,23 +10,48 @@ $user = new User($number);
 $pages = json_decode($_REQUEST['pages'], true);
 $title = $_GET['title'];
 if(isset($title)){
-  $page = get_wiki_page($title);
+  $sections = get_wiki_sections($title);
 }else{
   foreach($pages as $key=>$title){
     if($_REQUEST['Digits'] === $key);
-    $page = get_wiki_page($title);
+    $sections = get_wiki_sections($title);
   }
 }
+
+$say = '';
+$i = 0;
+$titles = [];
+foreach($sections as $name=>$section){
+  if(is_numeric($name)){
+    continue;
+  }
+  $say .= "For $name, dial $i. ";
+  $title[$i] = $name;
+  $i++;
+}
+
 $gather = $response->gather([
-  'action' => 'sections.php'
+  'action' => 'sections.php?title='.urlencode($title).'&titles='urlencode(json_encode($titles))
 ]);
 
-$gather->say($page,[
+$gather->say($sections[0],[
   'voice' => $user->voice,
   'language' => $user->language
 ]);
 
-function get_wiki_page($title){
+$gather->pause(3);
+
+$gather->say("More information on $title. ", [
+  'voice' => $user->voice,
+  'language' => $user->language
+]);
+
+$gather->say($say,[
+  'voice' => $user->voice,
+  'language' => $user->language
+]);
+
+function get_wiki_sections($title){
   $title = trim($title,'. ');
   $title = urlencode($title);
   $url = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles='.$title.'&exintro=1&explaintext=1&exsectionformat=plain&format=json';
@@ -40,6 +65,20 @@ function get_wiki_page($title){
     error_log(json_encode($page));
     $extract .= $page['extract'];
   }
-  return $extract;
+  
+  $parts = explode('\n\n\n', $extract);
+  $sections = [];
+  $i = 0;
+  foreach($parts as $part){
+    $pair = explode('\n',$part, 2);
+    if(count($pair) === 2){
+      list($key, $val) = $pair;
+      $sections[$key] = $val;
+    }else{
+      $sections[$i] = $pair[0];
+      $i++;
+    }
+  }
+  return $sections;
 }
 echo $response;
